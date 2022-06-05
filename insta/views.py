a29dc41e81880d51af4b510de1638cafe5f16741
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-05 20:12:13 +0300

    ft:method to get img that user follows

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,13 @@
+from turtle import title
 from django.shortcuts import render,redirect
 from .models import Image,Comment,Profile,User,Follow
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+#from .forms import NewImagePost,CreateComment,UpdateProfile
+from .models import Image,Comment,Profile,User,Follow
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
 
 
 
@@ -13,3 +21,11 @@ def index(request):
 	title = "Discover"
 	
 	return render(request,'index.html',{"images":images,"title":title})
+
+def timeline(request):
+	'''
+	Method that fetches imges from all the users that the current logged in user follows only
+	'''
+	follows = Follow.objects.filter(user_from = request.user.id)
+	images = Image.objects.filter(profile = request.user.following.user_to)
+	return render(request, 'accounts/timeline.html',{"images":images,"title":title})
