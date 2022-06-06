7fca6b49b4fcd30b8980b96807d137bb1928baac
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-06 15:08:56 +0300

    models

--- insta/admin.py
+++ insta/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import posts,Profile,Likes,Comment
 
-admin.site.register(posts)
 
 # Register your models here.
+admin.site.register(posts)
 admin.site.register(Profile)
 admin.site.register(Likes)
 admin.site.register(Comment)
